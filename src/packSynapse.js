88b95c81a37f9797357d3fbe7c958f3fdceda0eb
Chanel Thomas <chthomas@microsoft.com>
2022-09-30 13:56:25 -0400

    added forEach to allow for packed to work with multi config

--- src/packSynapse.js
+++ src/packSynapse.js
@@ -1,6 +1,6 @@
 const { getConfigId } = require("./getConfigId");
 const { parseXml } = require("./parseXml");
-const { selectConfigNode } = require("./selectConfigNodes");
+const { selectConfigNodes } = require("./selectConfigNodes");
 const { selectNameNode } = require("./selectNameNode");
 const { selectSchemaNode } = require("./selectSchemaNode");
 const { serializeXml } = require("./serializeXml");
@@ -11,33 +11,35 @@ function packSynapse({
   unpackedXml,
 }) {
   const dom = parseXml(unpackedXml);
-  const configNode = selectConfigNode(dom);
-  const configId = getConfigId(configNode);
-  const environmentConfig = unpackedEnvironmentJson[configId];
-  const synapseConfig = unpackedConfigJson[configId];
-  const nameNode = selectNameNode(configNode);
-  nameNode.textContent = environmentConfig.StorageAccountName;
-  const schemaJson = {};
-  schemaJson.SubscriptionId = environmentConfig.SubscriptionId;
-  schemaJson.ResourceGroupName = environmentConfig.ResourceGroupName;
-  schemaJson.StorageAccountName = environmentConfig.StorageAccountName;
-  schemaJson.BlobEndpoint = `https://${environmentConfig.StorageAccountName}.blob.core.windows.net/`;
-  schemaJson.QueueEndpoint = `https://${environmentConfig.StorageAccountName}.queue.core.windows.net/`;
-  schemaJson.TableEndpoint = `https://${environmentConfig.StorageAccountName}.table.core.windows.net/`;
-  schemaJson.FileEndpoint = `https://${environmentConfig.StorageAccountName}.file.core.windows.net/`;
-  schemaJson.FileSystemEndpoint = `https://${environmentConfig.StorageAccountName}.dfs.core.windows.net/`;
-  if (synapseConfig.IncludeWorkspace) {
-    schemaJson.SqlODEndpoint = `${environmentConfig.WorkspaceName}-ondemand.sql.azuresynapse.net`;
-    schemaJson.WorkspaceDevEndpoint = `https://${environmentConfig.WorkspaceName}.dev.azuresynapse.net`;
-  } else {
-    schemaJson.SqlODEndpoint = "";
-    schemaJson.WorkspaceDevEndpoint = "";
-  }
+  const configNodes = selectConfigNodes(dom);
+  configNodes.forEach((configNode) => {
+    const configId = getConfigId(configNode);
+    const environmentConfig = unpackedEnvironmentJson[configId];
+    const synapseConfig = unpackedConfigJson[configId];
+    const nameNode = selectNameNode(configNode);
+    nameNode.textContent = environmentConfig.StorageAccountName;
+    const schemaJson = {};
+    schemaJson.SubscriptionId = environmentConfig.SubscriptionId;
+    schemaJson.ResourceGroupName = environmentConfig.ResourceGroupName;
+    schemaJson.StorageAccountName = environmentConfig.StorageAccountName;
+    schemaJson.BlobEndpoint = `https://${environmentConfig.StorageAccountName}.blob.core.windows.net/`;
+    schemaJson.QueueEndpoint = `https://${environmentConfig.StorageAccountName}.queue.core.windows.net/`;
+    schemaJson.TableEndpoint = `https://${environmentConfig.StorageAccountName}.table.core.windows.net/`;
+    schemaJson.FileEndpoint = `https://${environmentConfig.StorageAccountName}.file.core.windows.net/`;
+    schemaJson.FileSystemEndpoint = `https://${environmentConfig.StorageAccountName}.dfs.core.windows.net/`;
+    if (synapseConfig.IncludeWorkspace) {
+      schemaJson.SqlODEndpoint = `${environmentConfig.WorkspaceName}-ondemand.sql.azuresynapse.net`;
+      schemaJson.WorkspaceDevEndpoint = `https://${environmentConfig.WorkspaceName}.dev.azuresynapse.net`;
+    } else {
+      schemaJson.SqlODEndpoint = "";
+      schemaJson.WorkspaceDevEndpoint = "";
+    }
 
-  Object.assign(schemaJson, synapseConfig);
-  delete schemaJson.IncludeWorkspace;
-  const schemaNode = selectSchemaNode(configNode);
-  schemaNode.textContent = JSON.stringify(schemaJson);
+    Object.assign(schemaJson, synapseConfig);
+    delete schemaJson.IncludeWorkspace;
+    const schemaNode = selectSchemaNode(configNode);
+    schemaNode.textContent = JSON.stringify(schemaJson);
+  });
   return serializeXml(dom);
 }
 exports.packSynapse = packSynapse;
